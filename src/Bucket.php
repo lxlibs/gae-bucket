5da7b253af109aaff632161fad35a060e7232f78
lenonleite <ex79236@edp.pt>
2019-02-11 11:47:59 +0000

    problem composer

--- src/Bucket.php
+++ src/Bucket.php
@@ -12,13 +12,89 @@ class Bucket
     private $filePath;
 
     /**
+     * @var array
+     */
+    private $options = [];
+
+    /**
+     * @var bool
+     */
+    private $isLocal;
+
+    /**
+     * @var string
+     */
+    private $error='';
+
+    /**
      * Bucket constructor.
      *
      * @param $filePath
+     * @param bool $isLocal
      */
-    public function __construct($filePath)
+    public function __construct($filePath, $isLocal = false)
     {
         $this->filePath = $filePath;
+        $this->isLocal = $isLocal;
+        $this->setOptions();
+        if(!$this->isBucket()){
+            $this->error = 'This is not bucket';
+        }
+
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return string
+     */
+    public function __call($name, $arguments)
+    {
+        if($this->error && $name !== 'setLocal'){
+            return $this->error;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function loadDefaultValues(){
+        $expireDate = \DateTime();
+        return [
+            'readCacheExpirySeconds' => Constants::DEFAULT_READ_CACHE,
+            'enableCache' => Constants::DEFAULT_CACHE_FLAG,
+            'metadataExpires' => $expireDate->format('Y-m-d\TH:i:sT'),
+            'metadataHits' => Constants::DEFAULT_CACHE_HITS_LIMIT
+
+        ];
+    }
+
+    /**
+     * @param array $options
+     * @param array $options - readCacheExpirySeconds int
+     * @param array $options - enableCache boleean
+     * @param array $options - metadataExpires DateTime format Y-m-d\TH:i:sT
+     * @param array $options - metadataHits int
+     *
+     * @return array
+     */
+    public function setOptions($options=[])
+    {
+        if(!empty($options['readCacheExpirySeconds'])){
+            $options['readCacheExpirySeconds'] = $this->getDateToOptions($options['readCacheExpirySeconds']);
+        }
+        $this->options = array_merge($this->loadDefaultValues(),$options);
+
+        return [
+            'gs' => [
+                'read_cache_expiry_seconds' => $this->options['readCacheExpirySeconds'],
+                'enable_cache' => $this->options['enableCache'],
+                'metadata' => [
+                    'expires' => $this->options['metadataExpires'],
+                    'hits' => $this->options['metadataHits']
+                ]
+            ],
+        ];
     }
 
     /**
@@ -26,8 +102,9 @@ class Bucket
      *
      * @return string
      */
-    public function getData()
+    public function get()
     {
+
         if (!file_exists($this->filePath)) {
             return null;
         }
@@ -56,29 +133,8 @@ class Bucket
         return file_exists(dirname($this->filePath));
     }
 
-    /**
-     * @param \DateTime $expireDate
-     * @param int $hits
-     *
-     * @return array
-     */
-    protected function buildMetaData($expireDate= null, $hits=0)
-    {
-        $expireDateString = $expireDate;
-        if ($expireDate instanceof \DateTime) {
-            $expireDateString = $expireDate->format('Y-m-d\TH:i:sT');
-        }
+    public function edit(){
 
-        return [
-            'gs' => [
-                'read_cache_expiry_seconds' => 0,
-                'enable_cache' => true,
-                'metadata' => [
-                    'expires' => $expireDateString,
-                    'hits' => $hits
-                ]
-            ],
-        ];
     }
 
     /**
@@ -91,10 +147,9 @@ class Bucket
      * @return bool
      * @throws \Exception
      */
-    public function setData( $value, $ttl = 0, $reads = 0, $metadata = null)
+    public function put( $value )
     {
-        $options = $this->mountOptions($ttl, $reads , $metadata);
-        $ctx = stream_context_create($options);
+        $ctx = stream_context_create($this->setOptions());
         $r = file_put_contents($this->filePath, $value, Constants::DEFAULT_VALUE_FLAG_FILE_PUT_CONTENTS, $ctx);
 
         if ($r !== false) {
@@ -105,35 +160,6 @@ class Bucket
     }
 
     /**
-     * @param $ttl
-     * @param $reads
-     * @param $metadata
-     * @return array
-     */
-    private function mountOptions($ttl, $reads , $metadata ){
-        if (!empty($metadata)) {
-            return $metadata;
-        }
-
-        $ttl = $this->getTimeCache($ttl);
-
-        return $this->buildMetaData($this->getDateToOptions($ttl), $reads);
-
-    }
-
-    /**
-     * @param int $timeInSeconds
-     * @return int
-     */
-    private function getTimeCache($timeInSeconds=0){
-        if(!empty($timeInSeconds)){
-            return $timeInSeconds;
-        }
-
-        return Constants::DEFAULT_CACHE_TTL_SECONDS;
-    }
-
-    /**
      * @param $timeSeconds
      * @return \LxLibs\Gae\DateTime
      */
@@ -141,5 +167,36 @@ class Bucket
         $date = new DateTime();
         $date->add(new DateInterval("PT{$timeSeconds}S"));
         return $date;
+    }
+
+    /**
+     * @param bool $local
+     */
+    private function isBucket(){
+        if($this->isLocal OR $this->checkRegexFileGae()){
+            return true;
+        }
+        return false;
+
+    }
+
+    /**
+     * @return bool
+     */
+    private function checkRegexFileGae(){
+        preg_match('/^gs:\/\//', $this->filePath, $matches, PREG_OFFSET_CAPTURE);
+        if(empty($matches[0])){
+            return false;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * @param bool $isLocal
+     */
+    public function setLocal($isLocal=true){
+        $this->isLocal = $isLocal;
     }
 }
--- src/Utils/Constants.php
+++ src/Utils/Constants.php
@@ -7,4 +7,6 @@ class Constants
     const DEFAULT_CACHE_TTL_SECONDS = 60;
     const DEFAULT_CACHE_HITS_LIMIT = 2000;
     const DEFAULT_VALUE_FLAG_FILE_PUT_CONTENTS = 0;
+    const DEFAULT_CACHE_FLAG = true;
+    const DEFAULT_READ_CACHE_EXPIRY = 0;
 }
